d8290457c2b06f9cb7542b1183e8c7df3ce8ce3a
Jeff <jwagense@ucsc.edu>
2020-08-01 21:41:29 -0700

    items fade out after delete btn clicked

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
+var items = document.getElementsByTagName("li");
+var icons = document.getElementsByTagName("span");
 
+// checks a element off
+for (let li of items) {
+	li.addEventListener("click", function () {
+		this.classList.toggle("checked");
+	});
+}
+
+// deletes a element in the list
+for (let i of icons) {
+	i.addEventListener("click", function () {
+		event.stopPropagation();
+		var fadeTarget = this.parentElement;
+
+		let fadeEffect = setInterval(function () {
+			if (!fadeTarget.style.opacity) {
+				fadeTarget.style.opacity = 1;
+			}
+			if (fadeTarget.style.opacity > 0) {
+				fadeTarget.style.opacity -= 0.1;
+			} else {
+				clearInterval(fadeEffect);
+				fadeTarget.remove();
+			}
+		}, 100);
+	});
+}
+
+/*var fadeEffect = setInterval(function () {
+    if (!fadeTarget.style.opacity) {
+        fadeTarget.style.opacity = 1;
+    }
+    if (fadeTarget.style.opacity > 0) {
+        fadeTarget.style.opacity -= 0.1;
+    } else {
+        clearInterval(fadeEffect);
+    }
+}, 200);*/
